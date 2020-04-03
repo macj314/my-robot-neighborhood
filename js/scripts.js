006639d013138c2c4fb7f6f0bb3b6ecc91b5291b
unknown <macjay314@gmail.com>
2020-04-03 09:56:09 -0700

    add skeleton function

--- js/scripts.js
+++ js/scripts.js
@@ -4,15 +4,28 @@ var boop = "boop";
 var neighb = "Won't you be my neighbor?";
 
 var messageConvert = function(int){
-
+  var outputArray = [];
+  for(i = 0; i <= int; i++){
+    if (i === 3){
+      outputArray.push(neighb);
+    }
+    else if (i === 2){
+      outputArray.push(boop);
+    }
+    else if (i === 1){
+      outputArray.push(beep);
+    }
+    else outputArray.push(i);
+  }
 }
 
 //User Logic
 $(document).ready(function(){
   $("#formOne").submit(function(){
     event.preventDefault();
-    var input = $("#user-input").val();
-    alert(typeof input);
+    var input = parseInt($("#user-input").val());
     messageConvert(input);
+
+    $("#output").text(messageConvert(input));
   })
 })
