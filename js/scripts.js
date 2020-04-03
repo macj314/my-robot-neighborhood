500b373a17f7389f7417ff2523f0222b8c8e163f
unknown <macjay314@gmail.com>
2020-04-03 14:47:34 -0700

    Add a second button and function to print out the result in a reverse order

--- js/scripts.js
+++ js/scripts.js
@@ -17,16 +17,43 @@ var messageConvert = function(int){
     else if (indexString.indexOf(1) > -1){
       outputArray.push(beep);
     }
-    else outputArray.push(i);
+    else {outputArray.push(i);}
   }
   return outputArray;
 }
 
-//User Logic
+var messageReverse = function(int){
+  var outputArray = [];
+  var indexString = "";
+  for(i = int; i >= 0; i--){
+    indexString = i.toString();
+    if (indexString.indexOf(3) > -1){
+      outputArray.push(neighbor);
+    }
+    else if (indexString.indexOf(2) > -1){
+      outputArray.push(boop);
+    }
+    else if (indexString.indexOf(1) > -1){
+      outputArray.push(beep);
+    }
+    else {outputArray.push(i);}
+  }
+  return outputArray;
+}
+
 $(document).ready(function(){
-  $("#formOne").submit(function(){
-    event.preventDefault();
-    var input = parseInt($("#user-input").val());
-    $("#output").text(messageConvert(input));
+  $("#button-one").click(function(){   
+      event.preventDefault();    
+      var input = parseInt($("#user-input").val());  
+      $("#output").text(messageConvert(input));
+      $("#form-one").submit()
   })
+  $("#button-two").click(function(){
+    event.preventDefault();    
+    var input = parseInt($("#user-input").val());  
+    $("#output").text(messageReverse(input));
+    $("#form-one").submit()
+  })
+
+
 })
